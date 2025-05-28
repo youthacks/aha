9dc7180f5e294e466d1fe054cddc54c56da27b9b
mattsoh <mattsoh100@gmail.com>
2025-05-28 16:22:24 +0100

    improved security

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
 class HomeController < ApplicationController
+  before_action :require_admin
   def index
     @admin = Admin.find_by(id: session[:admin_id]) # Check if an admin is logged in
   end
 
   def products
     @products = Product.all
+  end
+
+  private
+  def require_admin
+    if session[:admin_id].present? && Admin.exists?(session[:admin_id])
+      @admin = Admin.find(session[:admin_id])
+    end
   end
 end
