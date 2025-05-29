03aa19fd0771cf82e9706caf98a63c687b351972
mattsoh <mattsoh100@gmail.com>
2025-05-29 21:35:25 +0100

    trying to fix the logout button not working issue but now I give up and it's just a button that looks like a link for now

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import Rails from "@rails/ujs"
 
 const application = Application.start()
+Rails.start()
 
 // Configure Stimulus development experience
 application.debug = false
