4d1c89fd6c27251eed1810986838bb55be8ca78f
mattsoh <mattsoh100@gmail.com>
2025-05-30 17:12:53 +0100

    still trying to fix the damn sending email issue

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "aha@youthacks.org"
   layout "mailer"
+  
 end
--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,5 +1,7 @@
 class TestMailer < ApplicationMailer
     def sample_email(to_email)
+        headers["X-SES-CONFIGURATION-SET"] = "email-logging"
+        headers['X-SES-MESSAGE-TAGS'] = 'campaign=welcome'
         mail(to: to_email, subject: "Test Email from AWS SES") do |format|
             format.text { render plain: "This is a test email sent via AWS SES SMTP from Rails." }
         end
