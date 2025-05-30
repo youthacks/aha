4bac3ebab3c70a86b6522fc6bb194c751213e5f4
mattsoh <mattsoh100@gmail.com>
2025-05-30 23:54:44 +0100

    tiny fix...

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,6 @@
 class AdminMailer < ApplicationMailer
     def send_code(email, code)
         @code = code
-        mail(to: email, subject: 'Your Email Confirmation Code for Aha!)
+        mail(to: email, subject: 'Your Email Confirmation Code for Aha!')
     end
 end
