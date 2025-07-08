3c7a261272d938eaf28df7bdb181edaf1a78e145
Matthew Soh <matthew@mattsoh.dev>
2025-07-08 12:44:49 -0700

    fix

--- nest-deploy-aha.sh
+++ nest-deploy-aha.sh
@@ -13,7 +13,7 @@ git fetch origin main
 LOCAL_HASH=$(git rev-parse HEAD)
 REMOTE_HASH=$(git rev-parse origin/main)
 
-if [ "$LOCAL_HASH" == "$REMOTE_HASH" ] || ! docker ps -q -f name=mattsoh_aha | grep -q .; then
+if [ "$LOCAL_HASH" != "$REMOTE_HASH" ] || ! docker ps -q -f name=mattsoh_aha | grep -q .; then
 	echo "[$(date '+%Y-%m-%d %H:%M:%S')] New commit detected. Rebuilding and restarting Docker container..."
 	docker container prune -f 2>>"$ERROR_LOG"
 	# Check for disk space issues and prune builder cache if needed
