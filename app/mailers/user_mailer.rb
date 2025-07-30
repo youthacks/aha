7ec2cfac59f02be1329f40d66d40be020cdf928f
mattsoh <matthew@mattsoh.dev>
2025-07-30 21:02:46 +0000

    a fix

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-# app/mailers/user_mailer.rb
-class UserMailer < ApplicationMailer
-  default from: 'm'  # Custom sender
-
-  def welcome_email()
-    mail(to: "v+neya63b75avs@myunidays.com", subject: 'Welcome to Our App')
-  end
-end
